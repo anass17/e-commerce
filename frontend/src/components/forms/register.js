beac0a41536143a273d83cfac07149bf1ac20ef0
anass17 <anassboutaib2018@gmail.com>
2025-03-07 09:49:52 +0000

    Added Register Page

--- frontend/src/components/forms/register.js
+++ frontend/src/components/forms/register.js
@@ -1,12 +1,29 @@
 import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 import { TextField, Button, FormControl, InputLabel, Select, MenuItem, Checkbox, FormControlLabel, Grid2, Container, Typography } from '@mui/material';
 
+const getCSRFToken = () => {
+  return document.cookie
+    .split('; ')
+    .find(row => row.startsWith('XSRF-TOKEN='))
+    ?.split('=')[1];
+};
+
+const csrfToken = getCSRFToken();  // Get CSRF token from cookies
+
 function RegisterForm() {
+  const [status, setStatus] = useState({type: null, message: ''});
+
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
-    email: ''
+    email: '',
+    password: ''
   });
+
+  if (status.type == 'success') {
+    return (<Navigate to="/" />)
+  }
 
   const handleChange = (e) => {
     setFormData({
@@ -18,28 +35,37 @@ function RegisterForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    fetch('https://localhost:8000/register', {
+    fetch('http://localhost:8000/api/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'X-XSRF-TOKEN': csrfToken,
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(formData)
         })
         .then(response => response.json())
-        .then(data => console.log('Success:', data))
+        .then(data => {
+          setStatus({type: data.status, message: data.message});
+        })
         .catch(error => console.error('Error:', error));
   };
 
   return (
     <form onSubmit={handleSubmit} style={{margin: '60px 0'}}>
-    <Typography variant='h4' pb={2} component="h1" sx={{textAlign: 'center'}}>
-        Create an Account
-    </Typography>
-    <Typography variant='body1' pb={5} sx={{textAlign: 'center', color: '#555'}}>
-        Sign up today and become a part of our growing community!
-    </Typography>
-      <Container maxWidth="sm">
+      <Typography variant='h4' pb={2} component="h1" sx={{textAlign: 'center'}}>
+          Create an Account
+      </Typography>
+      <Typography variant='body1' pb={2} sx={{textAlign: 'center', color: '#555'}}>
+          Sign up today and become a part of our growing community!
+      </Typography>
+      <Typography variant='body1' pb={3} sx={{textAlign: 'center', color: 'red'}}>
+          {status.message}
+      </Typography>
+      <Container maxWidth="sm" pt={3}>
         <Grid2 container spacing={2}>
+
+        {/* <input type="hidden" name="_token" value="{{ csrf_token() }}"> */}
+
             <Grid2 size={{ xs: 12, sm: 6 }}>
                 <TextField
                     label="First Name"
@@ -80,6 +106,7 @@ function RegisterForm() {
                     fullWidth
                     type="password"
                     name="password"
+                    onChange={handleChange}
                 />
             </Grid2>
 
