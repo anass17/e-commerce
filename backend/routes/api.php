d3d18ca2472eec76355b5fc54849ee07732eefd7
anass17 <anassboutaib2018@gmail.com>
2025-03-07 09:48:54 +0000

    Added APi Routes

---
+++ backend/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register']);
