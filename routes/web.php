27300257287f04e6e313d652442b8cf657f91eaf
anass17 <anassboutaib2018@gmail.com>
2025-03-09 12:48:49 +0000

    Added routes for Product ressouce

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserController::class, 'index']);
+
+Route::resource('products', ProductController::class);
